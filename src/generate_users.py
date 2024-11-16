ab519ebb2c576a0fd87604559727f0526135a3dc
u-masao <4973920+u-masao@users.noreply.github.com>
2024-11-16 23:50:43 +0900

    bugfix, iter

--- src/generate_users.py
+++ src/generate_users.py
@@ -24,7 +24,7 @@ def generate_user_profiles(num_users=1000, categories=None):
     if categories is None:
         categories = []
 
-    user_ids = [for i in range(1, num_users + 1)]
+    user_ids = [i for i in range(1, num_users + 1)]
     genders = ["male", "female", "non-binary"]
 
     client = OpenAI(
