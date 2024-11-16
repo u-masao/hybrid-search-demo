3e7b3073d4815bc1f965087be0c8d2f9a24b49e8
u-masao <4973920+u-masao@users.noreply.github.com>
2024-11-16 19:45:52 +0900

    remove unused

--- src/embedding/embeddings.py
+++
@@ -1,7 +0,0 @@
-class Embedding:
-    def __init__(self, dimension):
-        self.dimension = dimension
-
-    def generate_embedding(self, text):
-        # Dummy implementation for generating an embedding
-        return [0.0] * self.dimension
