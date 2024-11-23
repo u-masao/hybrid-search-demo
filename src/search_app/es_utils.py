4608ddbf275d1268cbfcde755f539c81f292efe7
u-masao <4973920+u-masao@users.noreply.github.com>
2024-11-24 00:05:02 +0900

    add test for es_utils.

--- src/search_app/es_utils.py
+++ src/search_app/es_utils.py
@@ -1,15 +1,19 @@
 import os
-from elasticsearch import Elasticsearch
+
 from dotenv import load_dotenv
+from elasticsearch import Elasticsearch
 
 load_dotenv(".credential")
+
 
 def make_client(es_host):
     elastic_username = os.getenv("ELASTIC_USERNAME")
     elastic_password = os.getenv("ELASTIC_PASSWORD")
 
     if not elastic_username or not elastic_password:
-        raise ValueError("Elasticsearch credentials not set in environment variables.")
+        raise ValueError(
+            "Elasticsearch credentials not set in environment variables."
+        )
 
     return Elasticsearch(
         [es_host],
@@ -17,7 +21,10 @@ def make_client(es_host):
         ca_certs="certs/http_ca.crt",
     )
 
-def perform_vector_search(query_vector, item_index_name, user_index_name, top_k=5):
+
+def perform_vector_search(
+    query_vector, item_index_name, user_index_name, top_k=5
+):
     es = make_client("https://localhost:9200")
 
     def search_index(index_name):
@@ -40,15 +47,18 @@ def perform_vector_search(query_vector, item_index_name, user_index_name, top_k=
     user_results = search_index(user_index_name)
     return item_results, user_results
 
+
 def get_user_info(user_id, user_index_name):
     es = make_client("https://localhost:9200")
     response = es.get(index=user_index_name, id=user_id)
     return response["_source"]
 
+
 def get_item_info(item_id, item_index_name):
     es = make_client("https://localhost:9200")
     response = es.get(index=item_index_name, id=item_id)
     return response["_source"]
+
 
 def perform_translation_search(translation_vector, index_name, top_k=5):
     es = make_client("https://localhost:9200")
--- tests/test_search_app_es_utils.py
+++ tests/test_search_app_es_utils.py
@@ -1,18 +1,19 @@
-import pytest
-import os
 from elasticsearch import Elasticsearch
+
 from src.search_app.es_utils import (
-    make_client,
-    perform_vector_search,
-    get_user_info,
     get_item_info,
+    get_user_info,
+    make_client,
     perform_translation_search,
+    perform_vector_search,
 )
+
 
 def test_make_client():
     es_host = "https://localhost:9200"
     client = make_client(es_host)
     assert isinstance(client, Elasticsearch)
+
 
 def test_perform_vector_search():
     query_vector = [0.1] * 384
@@ -24,28 +25,45 @@ def test_perform_vector_search():
         query_vector=query_vector,
         item_index_name=item_index_name,
         user_index_name=user_index_name,
-        top_k=top_k
+        top_k=top_k,
     )
 
     assert len(item_results) <= top_k
     assert len(user_results) <= top_k
+
+
 def test_get_user_info():
-    user_id = 1
+    user_id = "A1WNRJMBau9a4UogThL4"
     user_index_name = "user_develop"
     user_info = get_user_info(user_id, user_index_name)
+    print(user_info)
     assert user_info is not None
-    assert "name" in user_info  # Assuming 'name' is a field in the user document
+    assert (
+        "sentence" in user_info
+    )  # Assuming 'name' is a field in the user document
+
 
 def test_get_item_info():
-    item_id = 5625149
+    item_id = "HFWNRJMBau9a4UogZhKy"
     item_index_name = "item_develop"
     item_info = get_item_info(item_id, item_index_name)
     assert item_info is not None
-    assert "title" in item_info  # Assuming 'title' is a field in the item document
+    assert (
+        "sentence" in item_info
+    )  # Assuming 'title' is a field in the item document
 
-def test_perform_translation_search():
-    translation_vector = [0.1] * 384
-    index_name = "translation_develop"
+
+def test_perform_user_translation_search():
+    translation_vector = [0.1] * 64
+    index_name = "user_develop"
+    top_k = 5
+    results = perform_translation_search(translation_vector, index_name, top_k)
+    assert len(results) <= top_k
+
+
+def test_perform_item_translation_search():
+    translation_vector = [0.1] * 64
+    index_name = "item_develop"
     top_k = 5
     results = perform_translation_search(translation_vector, index_name, top_k)
     assert len(results) <= top_k
