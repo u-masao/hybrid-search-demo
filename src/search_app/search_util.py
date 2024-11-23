cb5bcfd6a7606d1434196819354f453b42bd2503
u-masao <4973920+u-masao@users.noreply.github.com>
2024-11-23 22:24:54 +0900

    code formatting

--- src/search_app/search_util.py
+++ src/search_app/search_util.py
@@ -1,30 +1,40 @@
 import click
+
 from src.search_app.utils import make_client
 
+
 @click.command()
-@click.option('--es-host', required=False, default='https://localhost:9200/', help='Elasticsearch host URL.')
-@click.option('--index-name', required=True, help='Name of the Elasticsearch index.')
-@click.option('--query', required=True, help='Search query for the sentence, translation, or embedding fields.')
+@click.option(
+    "--es-host",
+    required=False,
+    default="https://localhost:9200/",
+    help="Elasticsearch host URL.",
+)
+@click.option(
+    "--index-name", required=True, help="Name of the Elasticsearch index."
+)
+@click.option(
+    "--query",
+    required=True,
+    help="Search query for the sentence, translation, or embedding fields.",
+)
 def search_db(es_host, index_name, query):
-    """Search the Elasticsearch index for the given query in sentence, translation, and embedding fields."""
+    """Search the Elasticsearch index for the given query in sentence, "
+    "translation, and embedding fields."""
     es = make_client(es_host)
 
     # Construct the search query
     search_query = {
-        "query": {
-            "multi_match": {
-                "query": query,
-                "fields": ["sentence"]
-            }
-        }
+        "query": {"multi_match": {"query": query, "fields": ["sentence"]}}
     }
 
     # Perform the search
     response = es.search(index=index_name, body=search_query)
 
     # Print the results
-    for hit in response['hits']['hits']:
-        print(hit['_source'])
+    for hit in response["hits"]["hits"]:
+        print(hit["_source"])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     search_db()
