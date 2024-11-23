60e4da95f0d2af590e65412125f21f33928664e7
u-masao <4973920+u-masao@users.noreply.github.com>
2024-11-23 23:24:45 +0900

    add embedding api test

--- src/embedding_api/client.py
+++ src/embedding_api/client.py
@@ -1,6 +1,9 @@
 import requests
 
-def text_embedding(text):
-    response = requests.post("http://localhost:5000/api/vector_search", json={"query_text": text})
-    return response.json()
 
+def text_embedding(
+    text: str, schema: str = "http", host: str = "localhost", port: int = 5001
+):
+    BASE_URL = f"{schema}://{host}:{port}"
+    response = requests.post(f"{BASE_URL}/embed", json={"query": text})
+    return response.json()
